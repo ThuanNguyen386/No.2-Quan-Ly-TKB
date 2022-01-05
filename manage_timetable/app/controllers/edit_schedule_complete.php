ce49ff4c809996bc701ff330a4f59cbc0d9a9788
triennt-2385 <nguyen.tien.trien@sun-asterisk.com>
2022-01-05 22:54:47 +0700

    edit schedule

---
+++ manage_timetable/app/controllers/edit_schedule_complete.php
@@ -0,0 +1,53 @@
+<?php 
+require_once './app/controllers/checkLogin.php';
+
+class EditScheduleComplete{
+    public function __construct(){
+        $_SESSION['edit-schedule-complete-notifi'] = "";
+        $data = isset($_SESSION['edit-schedule']) ? $_SESSION['edit-schedule'] : [];
+        $data[4] = $this-> createLessonFile($data[4]);// trả về tên file (tạo và ghi) tiết học 
+        $data[5] = $this-> createNoteFile($data[5]); // trả về tên file (tạo và ghi) note
+       
+
+        require_once './app/models/schedule.php';
+        $Schedule-> edit($data);
+    }
+
+    // Tạo file chứa thông tin Tiết học và notes
+    public function createLessonFile($data){
+        $lessonNameFile = "l". rand(1,99999) .".txt";
+        $path = "./web/file/lesson/". $lessonNameFile;
+        $fp = @fopen($path, "w+");
+        if(!$fp){
+            $_SESSION['edit-schedule-complete-notifi'] = "Không tạo được file";
+        }
+        else{
+            foreach($data as $row){
+                fwrite($fp, $row."\n");
+            }
+            fclose($fp);
+        }
+        return $lessonNameFile;
+
+    }
+
+    //Tạo file chưa thông tin Note
+    public function createNoteFile($data){
+        $noteNameFile = "n". rand(1,99999) .".txt";
+        $path = "./web/file/note/". $noteNameFile;
+        $fp = @fopen($path, "w+");
+        if(!$fp){
+            $_SESSION['edit-schedule-complete-notifi'] = "Không tạo được file";
+        }
+        else{
+            
+            fwrite($fp, $data);
+            
+            fclose($fp);
+        }
+        return $noteNameFile;
+    }
+}
+
+$EditScheduleComplete = new EditScheduleComplete();
+require_once './app/views/edit_schedule_complete.php';
