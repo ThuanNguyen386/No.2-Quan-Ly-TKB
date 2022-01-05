6c96c409035bfd7919d60ea5a9669872c6a2e5fd
DESKTOP-S09IBEJ\Dell <thuansahin@gmail.com>
2022-01-05 13:23:38 +0700

    Change data

---
+++ change_teacher_confirm.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<body>
+    <form action="change_teacher_complete.php"> 
+    <label id="Name">Họ và Tên</label><br>
+    <label id="Specialized">Bộ môn</label><br>
+    <label id="Degree">Học vị</label><br>
+    <label id="Avatar">Avatar</label><br>
+    <label id="Description">Mô tả thêm</label><br>
+    <input type="submit" value="Sửa lại">
+    <input type="submit" value="Đăng ký">
+    </form>
+</body>
+</html>
