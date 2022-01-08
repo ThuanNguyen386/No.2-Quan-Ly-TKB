146989b33cccb3fa9ba699b39e719f22b10858ea
Phạm Hà Thủy <66737002+hathuy55@users.noreply.github.com>
2022-01-08 20:56:58 +0700

    Add files via upload

---
+++ cuoiki/app/controller/SubjectComplete.php
@@ -0,0 +1,22 @@
+<?php
+    session_start();
+    require_once "../../app/common/ErrorValidate.php";
+    require_once "../../app/model/SubjectModel.php";
+    function getUrl() {
+        $urls = explode("/", filter_var(trim($_SERVER['PHP_SELF'], "/")));
+        $url = "/";
+        for($i = 0; $i < count($urls)-1; $i++){
+            $url = $url . $urls[$i] . "/";
+        }
+        return $url;
+    }
+    $name = $school_year = $description = $avatar = null;
+    $check = 0;
+    subjectAddComplete();
+    function subjectAddComplete(){
+        require_once "../../app/view/subject_add/SubjectAddComplete.php";
+        if (!$_SESSION['check-subject-add-complete']) {
+            header('Location:' . getUrl(). 'SubjectAdd/subjectAddInput');
+        } 
+    }
+?>
